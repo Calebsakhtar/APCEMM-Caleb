c31c88d32f4c301269694f082eacbc7ca76d1e57
ca525 <ca525@cam.ac.uk>
2024-06-24 17:45:17 +0100

    Add a batch script to set up the conda env

--- examples/HPC_Setup/setup-env.bat
+++ examples/HPC_Setup/setup-env.bat
@@ -1,10 +1,11 @@
+module purge
 module load miniconda/3
 conda init
 conda create --name APCEMM-caleb python=3.8
 conda activate APCEMM-caleb
-python -m pip3 install python3-dev
-python -m pip3 install netcdf4
-python -m pip3 install chaospy
-python -m pip3 install xarray
-python -m pip3 install matplotlib
-python -m pip3 install importlib-metadata==4.13.0  
+python -m pip install python3-dev
+python -m pip install netcdf4
+python -m pip install chaospy
+python -m pip install xarray
+python -m pip install matplotlib
+python -m pip install importlib-metadata==4.13.0  
