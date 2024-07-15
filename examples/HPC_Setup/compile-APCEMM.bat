57472b2503d48739df0b349f92c67c64636476f0
ca525 <ca525@cam.ac.uk>
2024-07-15 02:36:58 +0200

    Updated the APCEMM build instructions

--- examples/HPC_Setup/compile-APCEMM.bat
+++ examples/HPC_Setup/compile-APCEMM.bat
@@ -1,9 +1,16 @@
 # RUN THIS FROM THE APCEMM directory
+git clone https://github.com/Calebsakhtar/APCEMM.git
+cd APCEMM
+git pull
+git checkout ca525/perform-comparison
+git submodule update --init --recursive 
+mkdir build
+cd build
+cmake ../Code.v05-00/
+
 module purge
 module load gcc/11
 module load python/3.8
 module load cmake/latest
-mkdir build
-cd build
 cmake ../Code.v05-00/
 cmake --build .
--- examples/HPC_Setup/git-setup.bat
+++
@@ -1,7 +0,0 @@
-# DO NOT module purge
-# Run these commands in hpc-work
-git clone https://github.com/Calebsakhtar/APCEMM.git
-cd APCEMM
-git pull
-git checkout ca525/perform-comparison
-git submodule update --init --recursive 
