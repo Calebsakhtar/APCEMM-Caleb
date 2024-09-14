04160b404c29ee10d462030a495808e0acdfab33
ca525 <ca525@cam.ac.uk>
2024-09-14 17:50:35 +0100

    Add comparison code

---
+++ examples/parallel_mets/run_APCEMM_instance.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+if __name__ == "__main__":
+    inputpath = sys.argv[1]
+    casename = inputpath.split('/')[-1]
+    casename = casename.split("-")[0]
+
+    os.system(f"cp -r sample_rundir/ {casename}")
+    os.system(f"cp {inputpath} {casename}/met.nc")
+    os.system(f"cd {casename} && ./../../../build/APCEMM input.yaml")
