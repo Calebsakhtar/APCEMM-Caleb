2671ad033742ab741885a53d6f5e4e111796c176
ca525 <ca525@cam.ac.uk>
2024-09-15 21:27:10 +0100

    Add a fractional timestep example

---
+++ examples/parallel_fraction/run_APCEMM_instance.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+if __name__ == "__main__":
+    inputpath = sys.argv[1]
+    casename = inputpath.split('/')[-1]
+    casename = casename.split(".")[0] + "." + casename.split(".")[1]
+
+    print(f"Path: {inputpath}\n")
+    print(f"Casename: {casename}\n")
+    
+    os.system(f"cp -r sample_rundir/ {casename}")
+    os.system(f"cp {inputpath} {casename}/input.yaml")
+    os.system(f"cd {casename} && ./../../../build/APCEMM input.yaml")
