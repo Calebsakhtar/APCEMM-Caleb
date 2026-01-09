aea512e7ff1da1aa2a72280a0e1fc26370a21a59
Calebsakhtar <calebsakhtar@gmail.com>
2026-01-09 01:49:54 +0100

    Update the temperature perturbation

--- Code.v05-00/src/Core/LAGRIDPlumeModel.cpp
+++ Code.v05-00/src/Core/LAGRIDPlumeModel.cpp
@@ -333,6 +333,8 @@ void LAGRIDPlumeModel::initH2O() {
     auto& mask = maskInfo.first;
 
     if (optInput_.ADV_EP_HUM_FROM_FILE){
+        Vector_2D tempCustom = LAGRID::initTempCustom(xEdges_, yEdges_, optInput_.ADV_EP_TEMP_FILENAME);
+        met_.updateTempTotalFromCustom(tempCustom);
         // Load H2O field from file
         H2O_ = LAGRID::initH2OCustom(xEdges_, yEdges_, optInput_.ADV_EP_HUM_FILENAME);
 
